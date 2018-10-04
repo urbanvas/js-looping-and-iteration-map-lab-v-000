d5eff573c365d03e80f24d08bc74452a0e15a9be
Neil Seejoor <nvseejoor@gmail.com>
2018-10-04 20:43:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,6 @@
 function lowerCaseDrivers(arr) {
   return arr.map(el => {return el.toLowerCase()})
 }
-function nameToAttributes(arr) {
-  const objectDrivers = []
-  arr.map((el) => {objectDrivers.push(Object.assign({}, {firstName: el.split(' ')[0], lastName: el.split(' ')[1]}))}
-  return objectDrivers
-}
 
 function nameToAttributes(arr) {
   return arr.map(el => {
