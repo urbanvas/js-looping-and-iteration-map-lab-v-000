58a2dd1e48d347a3e92f8bbb27ee7d83104e027e
Neil Seejoor <nvseejoor@gmail.com>
2018-10-04 20:02:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,18 @@
 function lowerCaseDrivers(arr) {
   return arr.map(el => {return el.toLowerCase()})
 }
+function nameToAttributes(arr) {
+  for (const el in arr) {
+    arr[el].split(' ')
+  }
+}
+
+const drivers = [
+  'Bobby Smith',
+  'Sammy Watkins',
+  'Sally Jenkins',
+  'Annette Sawyer',
+  'Sarah Hucklebee',
+  'bobby ridge'
+];
+
