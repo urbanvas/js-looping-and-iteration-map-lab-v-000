b8b77043e64304447217906ec5ec77f47b4c7895
Neil Seejoor <nvseejoor@gmail.com>
2018-10-04 20:45:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ function lowerCaseDrivers(arr) {
 }
 
 function nameToAttributes(arr) {
-   arr.map(el => {
+  return arr.map(el => {
     return Object.assign({}, {firstName: el.split(' ')[0], lastName: el.split(' ')[1]})
   })
 }
 
 function attributesToPhrase(arr) {
-  
+
 }
